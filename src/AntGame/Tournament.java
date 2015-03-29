1d93fab1832445cb932100973ca12e00f295773d
Olivia <Olivia@Olivia-PC>
2015-03-29 16:02:20 +0100

    Started adding methods, haven't made it runnable yet :)

--- src/AntGame/Tournament.java
+++ src/AntGame/Tournament.java
@@ -4,7 +4,36 @@
  */
 package AntGame;
 
+import java.util.ArrayList;
+
 
 public class Tournament {
+    ArrayList<Integer> playerNo = new ArrayList<>();
+    ArrayList<AntBrain> brains = new ArrayList<>();
+    
+    public void Tournament(AntBrain[] uploaded){
+        int i =0;
+        
+        for(AntBrain b : uploaded){
+            brains.add(b);
+            playerNo.add(i);
+                    
+            i++;
+        }
+    }
+    
+    public AntBrain runTournament() {
+                
+        
+        return null;
+    }
+    
+    public ArrayList<AntBrain> runRound(){
+        int playersInRound = playerNo.size();
+        
+        
+        
+        return null;
+    }
     
 }
