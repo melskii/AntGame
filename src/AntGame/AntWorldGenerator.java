63ea34d5e60f50935ed88c471cb189bb07c4dc20
Toby <Toby@192.168.0.14>
2015-04-12 21:48:25 +0100

    World generation from file

--- src/AntGame/AntWorldGenerator.java
+++ src/AntGame/AntWorldGenerator.java
@@ -4,8 +4,12 @@
  */
 package AntGame;
 
+import AntGame.exceptions.AntWorldGeneratorException;
 import AntGame.exceptions.PositionException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
+
 
 
 public class AntWorldGenerator {
@@ -13,8 +17,138 @@ public class AntWorldGenerator {
     
     AntWorld antWorld;
     
-    public AntWorld antWorldGenerator(String file, boolean tournament)
+    public AntWorld antWorldGenerator(File a) throws IOException, AntWorldGeneratorException, PositionException  
     {
+        ArrayList<String> world = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileReader(a));
+        
+        String line = null;
+        
+        while ((line = br.readLine()) != null) {
+            
+            //System.out.println(line);
+            world.add(line);
+            
+        }
+        
+        String firstLine = world.get(0);
+        String secondLine = world.get(1);
+        
+        try {
+            int x = Integer.parseInt(firstLine);
+            int y = Integer.parseInt(secondLine);
+            antWorld = new AntWorld(x, y);
+            
+        } catch (NumberFormatException e) {
+            
+            throw new AntWorldGeneratorException("World file not valid: " + e.getMessage());
+        }
+        
+        world.remove(0);
+        world.remove(0);
+        
+        boolean even = true;
+        
+        for (int y = 0; y < world.size(); y++) {
+            even = (y % 2 == 0);
+            line = world.get(y);
+            StringBuilder sb = new StringBuilder(line);
+            int x = 0;
+            char position;
+            
+            while(line.length() > 0) {
+                
+                
+                position = line.charAt(0);
+                
+                //System.out.println(position);
+                
+                if (!even) {
+                    if(!Character.isWhitespace(position)) {
+                        throw new AntWorldGeneratorException("Every other line must start with a space");
+                    }
+                    else {
+                        sb.deleteCharAt(0);
+                        line = sb.toString();
+                        if (line.isEmpty()) {
+                            continue;
+                        }
+                        position = line.charAt(0);
+                    }
+                        
+                    
+                }
+                
+                if (x == 0) {
+                    if (position == '#') {
+                        try {
+                            antWorld.getPosition(x, y).setRocky();
+                            sb.deleteCharAt(0);
+                            line = sb.toString();
+                            x += 1;
+                            continue;
+                        } catch (PositionException ex) {
+                            
+                        }
+                    }
+                    else 
+                        throw new AntWorldGeneratorException("Perimeter must be rocky");
+                }
+                
+                if (position == '#') {
+                    antWorld.getPosition(x, y).setRocky();
+                    sb.deleteCharAt(0);
+                    line = sb.toString();
+                    x += 1;
+                    continue;
+                }
+                
+                if (position == '.') {
+                    sb.deleteCharAt(0);
+                    line = sb.toString();
+                    x += 1;
+                    continue;
+                }
+                
+                if (Character.isDigit(position)) {
+                    antWorld.getPosition(x, y).addFood(Character.getNumericValue(position));
+                    sb.deleteCharAt(0);
+                    line = sb.toString();
+                    x += 1;
+                    continue;
+                }
+                
+                if (position == '+') {
+                    antWorld.getPosition(x, y).setAntHill("Red");
+                    sb.deleteCharAt(0);
+                    line = sb.toString();
+                    x += 1;
+                    continue;                    
+                }
+                
+                if (position == '-') {
+                    antWorld.getPosition(x, y).setAntHill("Black");
+                    sb.deleteCharAt(0);
+                    line = sb.toString();
+                    x += 1;
+                    continue;                    
+                }
+                
+                if (Character.isWhitespace(position)) {
+                    sb.deleteCharAt(0);
+                    line = sb.toString();
+                    continue;
+                }
+                
+                else
+                    throw new AntWorldGeneratorException("Unknown character in world file!");
+                
+            }
+            
+        }
+        
+        
+        
         return antWorld;
     }
     
@@ -39,10 +173,10 @@ public class AntWorldGenerator {
                 }
             }
             
-            //generate where
-            //antWorld = hillGen(antWorld, x, y, "Black");
+            //generate where then make first anthill
+            //antWorld = hillGen(antWorld, 10, 10, "Black");
             
-            //generate where
+            //generate where then make second
             //antWorld = hillGen(antWorld, x, y, "Red");
             
             
@@ -74,7 +208,7 @@ public class AntWorldGenerator {
         return false;
     }
     
-    //generates anthill by horizontal lines from top left corner
+    //generates hexagon of side length 7 by horizontal lines from top left corner
     private AntWorld hillGen(AntWorld antWorld, int x, int y, String colour) {
         Position position = antWorld.getPosition(x, y);
         Position startOfLine;
@@ -82,30 +216,31 @@ public class AntWorldGenerator {
         int lengthLine = 7;
         for (int i = 0; i < 13; i++) {
             
-            if (i == 7) {
+            if (i == 6) {        //  i == 6 at the widest part of the hexagon so change direction
                 nextDir = 1;
             }
             
             position.setAntHill(colour);
+            //System.out.println(position);
             startOfLine = position;
             
             for (int j = 0; j < lengthLine - 1; j++) {
                 position = antWorld.adjacentCell(position, 0);
                 position.setAntHill(colour);
-                System.out.println("test1");
+                //System.out.println(position);
             }
             
             position = antWorld.adjacentCell(startOfLine, nextDir);
-            System.out.println("test2");
+            //System.out.println("test2");
             
             if (nextDir == 2) {
                 lengthLine += 1;
             }
-            else if (nextDir == 1) {
+            else if (nextDir == 1) { //if the direction has changed then each line needs to be smaller
                 lengthLine -= 1;
             }
-            System.out.println(i);
-            
+            //System.out.println(i);
+            //System.out.println(lengthLine);
         } 
         
         return antWorld;
@@ -116,7 +251,7 @@ public class AntWorldGenerator {
     private AntWorld foodGen(AntWorld antworld, int x, int y, int d) throws PositionException {
         Position position = antworld.getPosition(x, y);
         Position startOfLine;
-        int nextLine = (d + 1) % 6;
+        int nextLine = (d + 1) % 6; // one right turn 
         
         for (int i = 0; i < 5; i++) {
             
@@ -135,7 +270,4 @@ public class AntWorldGenerator {
         
         return antWorld;
     }
-    
-    
-    
 }
