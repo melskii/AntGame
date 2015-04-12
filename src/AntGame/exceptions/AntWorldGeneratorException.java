68b859b0c805ff93f79811059745bf7d4484c788
Toby <Toby@192.168.0.14>
2015-04-12 21:59:14 +0100

    added exception

---
+++ src/AntGame/exceptions/AntWorldGeneratorException.java
@@ -0,0 +1,12 @@
+
+package AntGame.exceptions;
+
+/**
+ *
+ * @author Toby
+ */
+public class AntWorldGeneratorException extends Exception {
+    
+     public AntWorldGeneratorException(String message) { super(message); }
+    
+}
